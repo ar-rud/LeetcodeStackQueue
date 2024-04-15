0618d74e70483ae1de1785077318c36e9f7f9589
artur_rudish <ar194225@gmail.com>
2024-04-16 00:32:39 +0300

    implemented maximum frequency stack and added photo proof

--- maximum_frequency_stack/maximum_frquency_stack.py
+++ maximum_frequency_stack/maximum_frquency_stack.py
@@ -1,3 +1,39 @@
 """
 Implementation of maximum frequency stack
 """
+from collections import deque
+
+class Node():
+    """node class"""
+    def __init__(self, val, freq=1) -> None:
+        self.val = val
+        self.freq = freq
+
+class FreqStack:
+
+    def __init__(self):
+        self.stack = deque()
+
+
+    def push(self, val: int) -> None:
+        for el in reversed(self.stack):
+            if el.val == val:
+                self.stack.append(Node(val, freq= el.freq + 1))
+                return
+        self.stack.append(Node(val))
+
+
+    def pop(self) -> int:
+        max_freq = 0
+        node = None
+        for nd in reversed(self.stack):
+            if nd.freq > max_freq:
+                max_freq = nd.freq
+                node = nd
+        self.stack.remove(node)
+        return node.val
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
